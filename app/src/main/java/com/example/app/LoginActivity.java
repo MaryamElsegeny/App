aa4ba7d5bf24aaaa82c11f2b43840edfe2dd4e35
maryam141 <maryam.elsegeny12@gmail.com>
2020-08-30 10:44:11 +0200

    new commit

--- app/src/main/java/com/example/app/LoginActivity.java
+++ app/src/main/java/com/example/app/LoginActivity.java
@@ -1,13 +1,32 @@
 package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //test
+
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this , MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
